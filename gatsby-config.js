733299beac9e19753d90f873bacdd6dd92048b21
Niclas Lövdahl <niclas.lovdahl@gmail.com>
2019-03-27 04:30:17 +0100

    Added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  plugins: [`gatsby-plugin-styled-components`]
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "UA-137021112-1"
+      }
+    }
+  ]
 };
