90d460a33d1f62b9b5b271d63b2718dad807efd9
Niclas Lövdahl <niclas.lovdahl@gmail.com>
2019-09-13 21:24:18 +0200

    Added web font loader and changed font
    Web font loader added to package.json. Page now uses font Roboto Mono.

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,3 +9,16 @@ module.exports = {
     }
   ]
 };
+
+module.exports = {
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-web-font-loader',
+      options: {
+        google: {
+          families: ['Roboto Mono']
+        }
+      }
+    }
+  ]
+}
